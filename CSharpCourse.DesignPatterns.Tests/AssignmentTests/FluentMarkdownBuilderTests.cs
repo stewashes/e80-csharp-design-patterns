ac380bf89cc8178fb6e29cfb453e7d58b9568358
Stefano Lavaggi <lavaggi.s@e80group.com>
2024-10-29 15:29:30 +0100

    assignment 1 done by Lavaggi Stefano

--- CSharpCourse.DesignPatterns.Tests/AssignmentTests/FluentMarkdownBuilderTests.cs
+++ CSharpCourse.DesignPatterns.Tests/AssignmentTests/FluentMarkdownBuilderTests.cs
@@ -14,7 +14,7 @@ public class FluentMarkdownBuilderTests
         ];
 
         var output = new FluentMarkdownBuilder()
-            // .AddTable(headers, rows)
+            .AddTable(headers, rows)
             .ToString();
 
         var expected = """
@@ -34,23 +34,23 @@ public class FluentMarkdownBuilderTests
         string[] headers = ["My", "Table"];
 
         var output = new FluentMarkdownBuilder()
-            //.AddTable(headers, table =>
-            //{
-            //    table.AddRow(row =>
-            //        {
-            //            row.AddCell(cell =>
-            //            {
-            //                cell.AddLink(link =>
-            //                {
-            //                    link.AddBold("Bold");
-            //                }, "https://example.com");
-            //            });
-            //            row.AddCell(cell =>
-            //            {
-            //                cell.AddItalic("Italic");
-            //            });
-            //        });
-            //})
+            .AddTable(headers, table =>
+            {
+                table.AddRow(row =>
+                    {
+                        row.AddCell(cell =>
+                        {
+                            cell.AddLink(link =>
+                            {
+                                link.AddBold("Bold");
+                            }, "https://example.com");
+                        });
+                        row.AddCell(cell =>
+                        {
+                            cell.AddItalic("Italic");
+                        });
+                    });
+            })
             .ToString();
 
         var expected = """
@@ -58,6 +58,96 @@ public class FluentMarkdownBuilderTests
                        |---|---|
                        |[**Bold**](https://example.com)|*Italic*|
 
+                       """;
+
+        Assert.Equal(expected, output);
+    }
+
+    [Fact]
+    public void CustomMarkdownTest()
+    {
+        string[] headers = ["My", "Test", "Table"];
+
+        var output = new FluentMarkdownBuilder()
+            .AddTable(headers, table =>
+            {
+                table.AddRow(row =>
+                {
+                    row.AddCell(cell =>
+                    {
+                        cell.AddLink(link =>
+                        {
+                            link.AddBold("test link bold");
+                        }, "https://example.com");
+                    });
+                    row.AddCell(cell =>
+                    {
+                        cell.AddItalic("italic");
+                    });
+                    row.AddCell(cell =>
+                    {
+                        cell.AddLink(link =>
+                        {
+                            link.AddItalic("test link italic");
+                        }, "https://example.com");
+                    });
+                });
+                table.AddRow(row =>
+                {
+                    row.AddCell(cell =>
+                    {
+                        cell.AddText("text1");
+                    });
+                    row.AddCell(cell =>
+                    {
+                        cell.AddItalic("text2");
+                    });
+                    row.AddCell(cell =>
+                    {
+                        cell.AddText("text3");
+                    });
+                });
+                table.AddRow(row =>
+                {
+                    row.AddCell(cell =>
+                    {
+                        cell.AddLink("text1", "www.text1.com");
+                    });
+                    row.AddCell(cell =>
+                    {
+                        cell.AddLink(link =>
+                        {
+                            link.AddText("text2");
+                        },"www.text2.com");
+                    });
+                    row.AddCell(cell =>
+                    {
+                        cell.AddLink("text3", "www.text3.com");
+                    });
+                });
+                table.AddRow(row =>
+                {
+                    row.AddCell(cell =>
+                    {
+                        cell.AddMonoSpace("mono");
+                    });
+                    row.AddCell(cell =>
+                    {
+                        cell.AddText("text");
+                    });
+                    row.AddEmptyCell();
+                });
+            })
+            .ToString();
+
+        var expected = """
+                       |My|Test|Table|
+                       |---|---|---|
+                       |[**test link bold**](https://example.com)|*italic*|[*test link italic*](https://example.com)|
+                       |text1|*text2*|text3|
+                       |[text1](www.text1.com)|[text2](www.text2.com)|[text3](www.text3.com)|
+                       |`mono`|text|---|
+                       
                        """;
 
         Assert.Equal(expected, output);
--- CSharpCourse.DesignPatterns/Assignments/FluentMarkdownBuilder.cs
+++ CSharpCourse.DesignPatterns/Assignments/FluentMarkdownBuilder.cs
@@ -4,45 +4,182 @@ namespace CSharpCourse.DesignPatterns.Assignments;
 
 internal class FluentMarkdownBuilder
 {
-    private readonly StringBuilder _builder = new();
+    protected readonly StringBuilder Builder;
+
+    public FluentMarkdownBuilder(StringBuilder? builder = null)
+    {
+        Builder = builder ?? new StringBuilder();
+    }
 
     public FluentMarkdownBuilder AddText(string text)
     {
-        _builder.Append(text);
+        Builder.Append(text);
         return this;
     }
 
     public FluentMarkdownBuilder AddHeader(int headerLevel, string text)
     {
         var prefix = new string(Enumerable.Repeat('#', headerLevel).ToArray());
-        _builder.AppendLine($"{prefix} {text}");
+        Builder.AppendLine($"{prefix} {text}");
         return this;
     }
 
     public FluentMarkdownBuilder AddBold(string text)
     {
-        _builder.Append($"**{text}**");
+        Builder.Append($"**{text}**");
         return this;
     }
 
     public FluentMarkdownBuilder AddItalic(string text)
     {
-        _builder.Append($"*{text}*");
+        Builder.Append($"*{text}*");
         return this;
     }
 
     public FluentMarkdownBuilder AddLink(string name, string url)
     {
-        _builder.Append($"[{name}]({url})");
+        Builder.Append($"[{name}]({url})");
         return this;
     }
 
     public FluentMarkdownBuilder NewLine()
     {
-        _builder.AppendLine();
+        Builder.AppendLine();
+        return this;
+    }
+
+    public FluentMarkdownBuilder AddMonoSpace(string text)
+    {
+        Builder.Append("`");
+        Builder.Append(text);
+        Builder.Append("`");
+        return this;
+    }
+
+    public FluentMarkdownBuilder AddTable(string[] header, string[][] rows)
+    {
+        var nColumns = header.Length;
+        
+        for(int i = 0; i < nColumns; i++)
+        {
+            Builder.Append("|");
+            Builder.Append(header[i]);
+        }
+
+        Builder.AppendLine("|");
+
+        //var delimiter = string.Concat(Enumerable.Repeat("|---", nColumns).ToArray());
+        //_builder.Append(delimiter);
+
+        for (int i = 0; i < nColumns; i++)
+        {
+            Builder.Append("|---");
+        }
+
+        Builder.AppendLine("|");
+
+        foreach (var i in rows)
+        {
+            foreach (var j in i)
+            {
+                Builder.Append("|");
+                Builder.Append(j);
+            }
+            
+            Builder.AppendLine("|");
+        }
+        
+        return this;
+    }
+
+
+    public FluentMarkdownBuilder AddTable(string[] headers, Action<TableBuilder> tableBuilderAction)
+    {
+        tableBuilderAction(new TableBuilder(Builder, headers));
         return this;
     }
 
     public override string ToString()
-        => _builder.ToString();
+        => Builder.ToString();
+}
+
+internal class TableBuilder : FluentMarkdownBuilder
+{
+    internal TableBuilder(StringBuilder builder, string[] header) : base(builder)
+    {
+        //for (int i = 0; i < header.Length; i++)
+        //{
+        //    Builder.Append("|");
+        //    Builder.Append(header[i]);
+        //}
+
+        //Builder.AppendLine("|");
+
+        //var delimiter = string.Concat(Enumerable.Repeat("|---", header.Length).ToArray());
+        //Builder.Append(delimiter);
+        
+        //Builder.AppendLine("|");
+
+        AddRow(row =>
+        {
+            for (int i = 0; i < header.Length; i++)
+            {
+                row.AddCell(cell => cell.AddText(header[i]));
+            }
+        });
+
+        AddRow(row =>
+        {
+            for (int i = 0; i < header.Length; i++)
+            {
+                // row.AddCell(cell => cell.AddText("---"));
+                row.AddEmptyCell();
+            }
+        });
+    }
+
+    public TableBuilder AddRow(Action<RowBuilder> rowBuilderAction)
+    {
+        rowBuilderAction(new RowBuilder(Builder));
+        Builder.AppendLine("|");
+        return this;
+    }
+}
+
+internal class RowBuilder: FluentMarkdownBuilder
+{
+    internal RowBuilder(StringBuilder builder) : base(builder) { }
+
+    public RowBuilder AddCell(Action<CellBuilder> cellBuilderAction)
+    {
+        Builder.Append("|");
+        cellBuilderAction(new CellBuilder(Builder));
+
+        return this;
+    }
+
+    public RowBuilder AddEmptyCell()
+    {
+        AddCell(cell => cell.AddText("---"));
+
+        return this;
+    }
+}
+
+internal class CellBuilder : FluentMarkdownBuilder
+{
+    internal CellBuilder(StringBuilder builder) : base(builder) { }
+
+    public CellBuilder AddLink(Action<FluentMarkdownBuilder> linkBuilderAction, string link)
+    {
+        Builder.Append("[");
+
+        linkBuilderAction(this);
+
+        Builder.Append("](");
+        Builder.Append(link);
+        Builder.Append(")");
+
+        return this;
+    }
 }
